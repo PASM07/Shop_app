d8d6ed24caec313aeb10cd36ea682b7a602a16b7
PASM07 <149417421+PASM07@users.noreply.github.com>
2023-10-31 13:28:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductTable from './ProductTable';
+import ProductPage from './ProductPage';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://dummyjson.com/products?limit=100')
+      .then((response) => {
+        setProducts(response.data.products);
+      })
+      .catch((error) => {
+        console.error('Ошибка при загрузке данных:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>E-Shop</h1>
+        <Routes>
+          <Route path="/" element={<ProductTable products={products} />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
