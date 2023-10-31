e7324067e8a905126af53e4c15956108921ca522
PASM07 <149417421+PASM07@users.noreply.github.com>
2023-10-31 16:15:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProductTable from './ProductTable';
 import ProductPage from './ProductPage';
 import axios from 'axios';
@@ -21,10 +21,10 @@ const App = () => {
   return (
     <Router>
       <div>
-        <h1>E-Shop</h1>
+      <h1><Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>E-Shop</Link></h1>
         <Routes>
-          <Route path="/" element={<ProductTable products={products} />} />
-          <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/" element={<ProductTable products={products} />} />
+        <Route path="/products/:title" element={<ProductPage products={products} />} />
         </Routes>
       </div>
     </Router>
